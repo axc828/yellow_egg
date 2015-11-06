3cf17543a6b83d6eb36ac81c6e43b948981fdf0a
Rita <Rita@LIN>
2015-11-06 17:42:51 +0800

    fix conflic

--- src/yellow_egg/HelloWorld.java
+++ src/yellow_egg/HelloWorld.java
@@ -1,10 +1,15 @@
 package yellow_egg;
 
 public class HelloWorld {
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("wwwwwww");
+		System.out.println("yyyyyyy");
 		System.out.println("Hello egg");
 	}
 
