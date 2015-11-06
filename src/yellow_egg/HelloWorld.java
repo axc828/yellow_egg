98a2eb8529d56d6f1c5bf9d711d9b8cba73d8ecb
pea <pea@pea-PC>
2015-11-06 18:02:40 +0800

    fix conflict

--- src/yellow_egg/HelloWorld.java
+++ src/yellow_egg/HelloWorld.java
@@ -7,10 +7,12 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		System.out.println("Hello World");
 		System.out.println("wwwwwww");
 		System.out.println("yyyyyyy");
 		System.out.println("Hello egg");
+
 	}
 
 }
